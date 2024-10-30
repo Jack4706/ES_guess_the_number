1eebeb763d35fa2c3d617ed2ff26a0024c81f20e
Jack <giacomonard@gmail.com>
2024-10-30 17:03:57 +0100

    Aggiunte difficolta

--- main.c
+++ main.c
@@ -5,51 +5,107 @@
 int main() {
     const int MIN = 0;
     const int MAX = 10;
-    int n, num;
+    int num;
     int tentativi = 0;
     int difficolta;
 
     srand(time(NULL));
-    n = rand() % (MAX - MIN + 1) + MIN;
+    int n = rand() % (MAX - MIN + 1) + MIN;
 
-    printf("Benvenuto in:\n     ***GUESS THE NUMBER***\n");
-    printf("    *seleziona difficolta*\n");
-    printf("");
-
-
-    switch(difficolta)
-
-
-
+    printf("Benvenuto in:\n     * * * G U E S S   T H E   N U M B E R * * *\n");
+    printf("\n");
+    printf("              seleziona difficolta:\n");
+    printf("\n");
+    printf("1.                 * FACILE *\n2.                * * MEDIO * *\n3.            * * * DIFFICILE * * *\n");
+    printf("\n");
+    scanf("%d", &difficolta);
 
 
+    switch (difficolta) {
+        case 1:
+            printf("                   * FACILE *\n                 hai 3 tentativi\n");
+            do {
+                printf("\n          Indovina il numero tra %d e %d\n", MIN, MAX);
+                scanf("%d", &num);
 
+                if (num < MIN || num > MAX)
+                    printf("      Errore! inserire un numero da %d a %d\n", MIN, MAX);
 
-    do {
-        printf("\nIndovina il numero tra %d e %d:\n", MIN, MAX);
-        scanf("%d", &num);
+                else if (num > n) {
+                    printf("                    SBAGLIATO\n              HINT: il numero e' <\n");
+                    tentativi++;
+                } else if (num < n) {
+                    printf("                    SBAGLIATO\n              HINT: il numero e' >\n");
+                    tentativi++;
+                } else if (num == n) {
+                    printf("             Hai indovinato il numero\n");
+                    printf("\n");
+                    printf("                !!V I C T O R Y!!\n");
+                } else {
+                    printf("Hai sbagliato");
+                }
+            } while (num != n && tentativi < 3);
 
-        if (num < MIN || num > MAX)
-            printf("Errore! inserire un numero da %d a %d\n", MIN, MAX);
+            if (tentativi == 3) {
+                printf("\n            Hai esaurito i tentativi\n              !!G A M E   O V E R!!\n");
+            }
+            break;
+        case 2:
+            printf("                  * * MEDIO * *\n                 hai 2 tentativi\n");
+            do {
+                printf("\n          Indovina il numero tra %d e %d\n", MIN, MAX);
+                scanf("%d", &num);
 
-        else if (num > n) {
-            printf("Sbagliato\nAiuto: il numero e' <\n");
-            tentativi++;
-        } else if (num < n) {
-            printf("Sbagliato\nAiuto: il numero e' >\n");
-            tentativi++;
+                if (num < MIN || num > MAX)
+                    printf("      Errore! inserire un numero da %d a %d\n", MIN, MAX);
 
-        } else if (num == n) {
-            printf("\nHai indovinato il numero\n     !!VICTORY!!");
-        } else {
-            printf("Hai sbagliato");
-        }
+                else if (num > n) {
+                    printf("                    SBAGLIATO\n              HINT: il numero e' <\n");
+                    tentativi++;
+                } else if (num < n) {
+                    printf("                    SBAGLIATO\n              HINT: il numero e' >\n");
+                    tentativi++;
+                } else if (num == n) {
+                    printf("             Hai indovinato il numero\n");
+                    printf("\n");
+                    printf("                !!V I C T O R Y!!\n");
+                } else {
+                    printf("Hai sbagliato");
+                }
+            } while (num != n && tentativi < 2);
 
-    } while (num != n && tentativi < 3);
+            if (tentativi == 2) {
+                printf("\n            Hai esaurito i tentativi\n              !!G A M E   O V E R!!\n");
+            }
+            break;
+        case 3:
+            printf("              * * * DIFFICILE * * *\n                 hai 1 tentativo\n");
+            do {
+                printf("\n          Indovina il numero tra %d e %d\n", MIN, MAX);
+                scanf("%d", &num);
 
-    if (tentativi == 3) {
-        printf("\nHai perso tutte le vite\n     !!GAME OVER!!\n");
+                if (num < MIN || num > MAX)
+                    printf("      Errore! inserire un numero da %d a %d\n", MIN, MAX);
+
+                else if (num > n) {
+                    printf("                    SBAGLIATO\n");
+                    tentativi++;
+                } else if (num < n) {
+                    printf("                    SBAGLIATO\n");
+                    tentativi++;
+                } else if (num == n) {
+                    printf("             Hai indovinato il numero\n");
+                    printf("\n");
+                    printf("                !!V I C T O R Y!!\n");
+                } else {
+                    printf("Hai sbagliato");
+                }
+            } while (num != n && tentativi < 1);
+
+            if (tentativi == 1) {
+                printf("\n            Hai esaurito i tentativi\n              !!G A M E   O V E R!!\n");
+            }
+            break;
     }
-
     return 0;
 }
